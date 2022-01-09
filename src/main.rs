ef009b30cae7b4da6727438202f739916802bbc9
win10_banana <tomsawyer404@outlook.com>
2022-01-09 12:47:49 +0800

    feat: Add valid check for url and kv-pair

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::{Parser, Subcommand};
+use anyhow::{anyhow, Result};
+use reqwest::Url;
+use std::str::FromStr;
 
 // A naive httpie implementation with Rust, can you imagine how easy it is?
 #[derive(Parser, Debug)]
@@ -18,15 +21,53 @@ enum SubCommands {
 /// Feed get with an url and we will retrieve the response for you
 #[derive(Parser, Debug)]
 struct Get {
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
+    Ok(s.into())
 }
 
 /// Feed post with an url and optional `key=value` pairs. We will post the data
 /// as JSON, and retrieve the response for you
 #[derive(Parser, Debug)]
 struct Post {
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
-    body: Vec<String>
+
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body: Vec<KvPair>
+}
+
+/// The `key=value` pair in command line can parse into `KvPair` struct using `parse_kv_pair`
+#[derive(Debug)]
+struct KvPair {
+    k: String,
+    v: String
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // use `=` to split, we will get a iterator
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self{
+            // Get first result from iterator as `key`, the iterator return `Some(T)/None`
+            // We transform `Ok(T)/Err(E)`, then use `?` to deal with error
+            k: (split.next().ok_or_else(err)?).to_string(),
+
+            // Get second result from iterator as `value`
+            v: (split.next().ok_or_else(err)?).to_string()
+        })
+    }
+}
+
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
 }
 
 fn main() {
